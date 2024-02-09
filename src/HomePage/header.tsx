a6133117dc09736c453cb7a57987e99e8355276f
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2024-02-09 22:34:48 +0100

    animation correction Feb 9 22:34

--- src/HomePage/header.tsx
+++ src/HomePage/header.tsx
@@ -21,7 +21,7 @@ const frames = keyframes`
  
 
   100%{
-     transform:translate(calc(-100% - 0.25rem))
+     transform:translate(-100%)
   }
 
 
@@ -86,7 +86,7 @@ Built Footwear. </span>
 Excellence. </span> <span className='text-[#EEFFFF]'> Step Into the Future of
 Fitness.  </span><span> Order Yours Now and
 Experience the Power </span>
-<span className='text-[#EEFFFF] mr-1'> of Purpose
+<span className='text-[#EEFFFF]'> of Purpose
 Built Footwear.</span>
 </div>
 
@@ -103,7 +103,7 @@ Cutting-Edge Shoes! </span><span>Elevate Your Performance,</span>
 <div className="content w-fit">
   <span>Unleash Your Ultimate Stride with </span> <span className='text-[#EEFFFF]'>Our
 Cutting-Edge Shoes! </span><span>Elevate Your Performance,</span>
-<span className='text-[#EEFFFF] mr-1'>Embrace Comfort and Conquer Every Run.</span>
+<span className='text-[#EEFFFF]'>Embrace Comfort and Conquer Every Run.</span>
 </div>
 
 </div>
